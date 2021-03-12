dcc25c658632f4c9257b51465b9d5ca43fab6f31
yaosamo <yaosamo@gmail.com>
2021-03-11 22:35:04 -0800

    og test

--- nextjs-blog/.next/static/chunks/webpack.js
+++ nextjs-blog/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7225bf1c34fefa11d241";
+/******/ 	var hotCurrentHash = "fcf70fd4de40ef8209d6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -12,7 +12,7 @@ export default function Podcast() {
         <meta property="og:url" content="https://yaosamo.com" />
         <meta property="og:title" content="Podcast 'And Designer'" />
         <meta property="og:description" content="Here we expose designs to other areas such as engineering, management, business and all other aspects related to design. Hosted by Yaric Samoylov." />
-        <meta property="og:image" content="../hi.jpg" />
+        <meta property="og:image" content="https://www.apple.com/v/iphone-12/f/images/specs/dimensions_iphone12__da0hivvaydw2_large_2x.jpg" />
       </Head>
 
       <div className="header">
