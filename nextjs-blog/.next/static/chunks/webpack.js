7c97f552550e66f16e90282424803db98ca08551
yaosamo <yaosamo@gmail.com>
2021-03-11 22:21:01 -0800

    changes for next

--- nextjs-blog/.next/static/chunks/webpack.js
+++ nextjs-blog/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5f999f210eb32846bef8";
+/******/ 	var hotCurrentHash = "3b56f5e071f6ea94cc0c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
