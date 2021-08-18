e28c4b2cd00b85ff39d1521e68e7b73de7eedf78
Yaroslav Samoylov <yaroslavsamoylov@Yaroslavs-MacBook-Air.local>
2021-08-17 20:19:16 -0700

    small text tweak

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5261e854c8d4ac4da509"; }
+/******/ 		__webpack_require__.h = function() { return "ec323dd628cac6c9d384"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
