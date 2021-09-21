9f04a6c89a007495cbff5d936a4ab10dd17619db
Yaroslav Samoylov <yaroslavsamoylov@Yaroslavs-MacBook-Air.local>
2021-09-21 07:25:02 -0700

    next.js config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,38 @@
-const withImages = require('next-images')
+// const webpack = require('webpack');
+// const withImages = require('next-images')
 
-module.exports = {
-    ...withImages(),
-    future: {
-        webpack5: true,
+// module.exports = {
+//     ...withImages(),
+//     future: {
+//         webpack5: true,
+//     },
+
+// }
+const webpack = require('webpack')
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+const path = require('path');
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isProduction = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    webpack: (config, { dev }) => {
+        config.plugins.push(
+            new webpack.EnvironmentPlugin(process.env),
+        );
+        
+        config.resolve.alias['components'] = path.join(__dirname, 'components')
+        config.resolve.alias['static'] = path.join(__dirname, 'static')
+
+        return config;
     },
+};
 
-}
+module.exports = withPlugins([
+    [optimizedImages, {
+        handleImages: ['jpeg', 'png'],
+    }],
+    
+    ],
+    nextConfig
+);
