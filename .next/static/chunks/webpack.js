78e9d3c07e36c3138285fba21cf93923decb3b39
Yaroslav Samoylov <yaosamo@gmail.com>
2021-10-04 22:06:55 -0700

    about progress

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "283b91e3cc4115ece9ca"; }
+/******/ 		__webpack_require__.h = function() { return "7afdb457325546a15acb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/about.js
+++ pages/about.js
@@ -35,7 +35,7 @@ export default function Podcast() {
         <meta name="msapplication-TileColor" content="#fafafa"/>
         <meta name="msapplication-TileImage" content="/static/meta/ms-icon-144x144.png" />
         <meta name="theme-color" content="#fafafa" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet"></link>
       </Head>  
       
       <Link href="/">
@@ -43,24 +43,99 @@ export default function Podcast() {
               <img src="../images/home/Arrow-right.svg" className="arrow"/>
             </div></Link>
             <div className="content">
-      <div className="header">
-        <p className="timestamp">October 3, 2021 at 9:43 PM</p>
-        <p className="subtitle">Hi, I’m a curious designer working on tools for dealerships at Gubagoo. Previously Zvooq, Genue.
-Born and raised in Togliatti, Russia. Living in a beautiful Hillsboro, Oregon. I’m an O1 visa holder and currently working on the greencard. Married to my beautiful wife and we have almost 3 years old dragon-blood son.
-I love how through design you can change industries. I’m always been inspired by the tech to create someting new. I’m excited to see what is interfaces of the future will look likeOn my spare time I enjoy adjacent areas to design such as 3D, coding, art and game development.
-I play ultimate freesbi every weekend. Also I watch english premier league and cheer for the Arsenal.
-Random, but I recently discovered that my parents gave me a name that is well designed. </p>
-      </div>
-      </div>
-   
+                <div className="item-1">
+                  <p className="timestamp">October 3, 2021 at 9:43 PM</p>
+                  <p className="subtitle">Hi, I’m a curious designer working on tools for dealerships at Gubagoo. Previously Zvooq, Genue.
+          Born and raised in Togliatti, Russia. Living in a beautiful Hillsboro, Oregon. I’m an O1 visa holder and currently working on the greencard. Married to my beautiful wife and we have almost 3 years old dragon-blood son.
+          I love how through design you can change industries. I’m always been inspired by the tech to create someting new. I’m excited to see what is interfaces of the future will look likeOn my spare time I enjoy adjacent areas to design such as 3D, coding, art and game development.
+          I play ultimate freesbi every weekend. Also I watch english premier league and cheer for the Arsenal.
+          Random, but I recently discovered that my parents gave me a name that is well designed. </p>
+                <div><img src="../images/about/name.svg"/></div>
+                <h2>I like to design things for my close friends</h2>
+                <h2>Links</h2>
+                <ul>
+                  <li>Twitter @yaosamo</li>
+                  <li>GitHub @yaosamo</li>
+                  <li>Instagram @yaosamo</li>
+                  <li>Spotify @yaosamo</li>
+                  <li>Email hi@yaosamo.com</li>
+                </ul>
+                <p>Thank you very much for visiting! Also I do not use tracking of anykind on this website. Enjoy and be safe!</p>
+                </div> 
+
+              <div className="item-2">
+              <div><img className="me" src="../images/about/me.webp"/></div>
+              <p>This is me working on this page, while listening Sludge playlist and watching Silicon Valley on the background</p>
+              <h2>My tools and setup</h2>
+              <ul>
+                <li>Software</li>
+                <li>Figma, duh</li>
+                <li>Notion</li>
+                <li>Github</li>
+                <li>Visual Studio Code</li>
+                <li>Browsers: Safari, Brave</li>
+                <li>UsabilityHub</li>
+                <li>Principle for mac</li>
+                <li>Terminal</li>
+                <li>Blender</li>
+                <li>Telegram</li>
+                <li>Construct 3</li>
+                <li>Vercel</li>
+                <li>iCloud 2tb</li>
+                </ul>
+
+                <ul>
+                <li>Hardware</li>
+                <li>iMac Pro 2017 (Xeon 8 core, 1tb, 32gb, Radeon Vega 56 8gb)</li>
+                <li>Macbook Air 2021 (M1, 16gb, 256gb)</li>
+                <li>Airpods Pro Max</li>
+                <li>Airpods Pro</li>
+                <li>iPhone XS Max 512gb</li>
+                <li>Redmi note 7</li>
+                <li>Wacom Intuos BT</li>
+                <li>Microphone</li>
+                </ul>
+
+                <ul>
+                <li>Products I especially adore</li>
+                <li>Bellroy</li>
+                <li>Airbirds</li>
+                <li>Sputnik bags</li>
+                <li>Shwood Eyewear</li>
+                </ul>
+                
+                </div>
+              </div>
+            
       
+   
+       
       
      
       <style jsx>{`
         .content {
-          border: 1px solid red;
+          display: grid;
+          grid-template-columns: repeat(6, 1fr);
+          grid-gap: 5rem;
         }  
-    
+
+        .content ul {list-style-type: none; padding-inline: 0;}
+
+        .item-1 {grid-column: 1 / 6;}
+        .item-2 {width: 24rem}
+
+        @media (max-width: 414px) {
+          .content {grid-template-columns: 1fr;}
+          .item-1 {grid-column: 1 / 1;}
+        }
+
+        .me {width:100%}
+        
+        .column {
+          width: 24rem;
+          border: 1px dashed red;}
+
+
         .arrow-shape {
           background: var(--gray-10);
           width: 2.5rem;
@@ -111,24 +186,6 @@ Random, but I recently discovered that my parents gave me a name that is well de
         p {
           margin-block-start: 0em;
           margin-block-end: 0em;
-        }
-
-
-        .column {
-          background-color: var(--element-background);
-          border-radius: 1rem;
-          display: flex;
-          flex-direction: column;
-          padding: 8px;
-          margin-block-end: 1rem;
-          transition: all .1s;
-        }
-
-        .column:hover {
-          cursor: pointer;
-          transform: translateY(-1px);
-          background: var(--element-hover);
-          box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.05);
         }
 
         .wrapper h1 {
