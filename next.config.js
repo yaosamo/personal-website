1cef10d4d03910146310a620dbb2d5e8cd9c44f1
Yaroslav Samoylov <yaosamo@gmail.com>
2022-03-23 21:32:29 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
-
 module.exports = {
-    images: {
-      domains: ['res.cloudinary.com'],
-    },
-  };
-
-  rewrites() {
-    return {
-        beforeFiles: [
-            // if the host is `app.acme.com`,
-            // this rewrite will be applied
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: 'chameleon.yaosamo.com',
-                    },
-                ],
-                destination: 'yaosamo.com/things/chameleon',
-            },
-        ]
-    }
-  }
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
