7cc536480c7854a6bc6c80213e0848040ffcabc6
Yaroslav Samoylov <yaosamo@gmail.com>
2022-09-17 00:01:13 -0700

    case structure

--- components/case.js
+++ components/case.js
@@ -1,21 +1,36 @@
 const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
+import styles from "../components/case.module.css";
 
 export default function Case() {
   return (
-    <div>
-      <p>2019-2022 / Gubagoo -> Released</p>
-      <h1 className="headingL weightMedium">
-        A service to sell cars fully online. Clients include Audi, BMW, Land
-        Rover, Nissan, Toyota, Volvo, Porsche and other brands
-      </h1>
-      <p>
-        Product design: Emrah Demirag, Eugene Belyakoff, Tim Marks, Yaroslav
-        Samoylov
-      </p>
-      <p>
-        Direction: Alyssa Bouranova, John Ellithorp, Miran Maric and whole
-        Asbury AG, Ryan Osten, Tim Marks
-      </p>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <p className="bodyMD">2019-2022 / Gubagoo -> Released</p>
+        <h1 className="headingL weightMedium">
+          A service to sell cars fully online. Clients include Audi, BMW, Land
+          Rover, Nissan, Toyota, Volvo, Porsche and other brands
+        </h1>
+        <div className={styles.contributors}>
+          <p className="bodyMD">
+            Product design: Emrah Demirag, Eugene Belyakoff, Tim Marks, Yaroslav
+            Samoylov
+          </p>
+          <p className="bodyMD">
+            Direction: Alyssa Bouranova, John Ellithorp, Miran Maric and whole
+            Asbury AG, Ryan Osten, Tim Marks
+          </p>
+        </div>
+      </div>
+      <div className={styles.items}>
+        {/* Individual pics */}
+        <div className={styles.item}>
+          <div className={styles.images}>
+            <img width="428" height="844" />
+            <img width="428" height="844" />
+          </div>
+          <p>Title</p>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../components/layout";
-import styles from "../styles/index.module.css";
+import styles from "../components/index.module.css";
 import Case from "../components/case";
 
 export default function Home() {
