d9a3aa9717b906ec4d725dc851202799e847e450
Yaroslav Samoylov <yaosamo@gmail.com>
2022-11-03 20:36:21 -0700

    img opt

--- components/customcode/chameleonscroller.js
+++ components/customcode/chameleonscroller.js
@@ -14,20 +14,20 @@ export default function Chameleonscroller() {
         src="images/work/case-two/item-2/vid-1.mp4"
       />
       <p className={styles.copy}>
-        I had a problem using one audioplayer and its scroll bar was small and
-        wasn't comfortable scrolling through my 10h+ long books. It was hard to
-        be presice when scatter for a certain moment. I wanted to be able to
-        scrub through an audio book for 10s or 4h in a convenient way.
+        I had a problem while using an audio player: its scroll bar was small
+        and wasn't comfortable when scrolling through my 10h+ long books. It was
+        hard to be precise when scanning for a certain moment. I wanted to be
+        able to scrub through an audio book for 10s or 4h in a convenient way.
         <br />
         <br />
-        So I designed custom scroller. Its width equals to the lenght of a book
-        in seconds the bar goes all the way off the screen and it scrolls using
-        native iOS ScrollView which allow you to scroll very slow and very fast
-        with few swipes.
+        My solution was to design a custom scroller. Its width is equal to the
+        length of a book in seconds, so the bar goes all the way off the screen,
+        and it scrolls using the native iOS ScrollView – which allows you to
+        scroll both very slow and very fast with few swipes.
         <br />
         <br />
-        This way scattering is joyful and easy on any lengh, seconds, minutes or
-        hours.
+        This way scanning is joyful and easy for any amount of time: seconds,
+        minutes or hours.
       </p>
     </>
   );
