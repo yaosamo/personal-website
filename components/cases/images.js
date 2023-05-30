e9ef0069a9dc0d2460916b310a7c4eaef2cae114
Yaroslav Samoylov <yaosamo@gmail.com>
2023-05-29 20:52:52 -0700

    new work added

--- components/cases/images.js
+++ components/cases/images.js
@@ -2,12 +2,9 @@ import styles from "../cases/case.module.css";
 import { useState } from "react";
 
 export default function Images({ Item, Img, i }) {
-  const [eggA, setEggA] = useState(false);
-
   return (
     <img
       src={Img.img}
-      className={eggA ? styles.eastereggA : ""}
       style={Item.maxwidth ? { maxWidth: Item.maxwidth } : {}}
     />
   );
